934bf5b1567f1605a670213d52334f8f1176382f
Ian Tauro <sushruth127@gmail.com>
2024-01-01 17:24:46 +0530

    Setup express server

---
+++ server.js
@@ -0,0 +1,24 @@
+// Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Server Setup
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("/public"));
+
+// View engine and views path
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// GET routes
+app.get("/", (req, res) => {
+    res.send("index");
+});
+
+// Run Server using the http socket server created.
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
