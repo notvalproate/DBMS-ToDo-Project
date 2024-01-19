a3bd6f422f19efa6bfd5076b27e305c7a84821c9
Ian Tauro <sushruth127@gmail.com>
2024-01-19 21:15:38 +0530

    Added re-enter password validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -32,4 +32,48 @@ $(document).ready(() => {
             repasswordInput.attr("type", "password");
         }
     });
+
+
+    const form = $(".login-box");
+    const submitButton = $(".submit-button");
+    const repasswordInput = $("#repassword");
+    const passwordInput = $("#password");
+
+    let passwordMatching = false;
+
+    passwordInput.on("input", (e) => {
+        e.target.value = removeSpaces(e.target.value);
+        if(e.target.value.length === 0) {
+            repasswordInput.removeClass("password-wrong")
+        }
+
+        if($("#password").val() !== $("#repassword").val()) {
+            passwordMatching = false;
+        } else {
+            passwordMatching = true;
+        }
+    })
+
+    repasswordInput.on("input", (e) => {
+        e.target.value = removeSpaces(e.target.value);
+        if($("#password").val() !== $("#repassword").val()) {
+            repasswordInput.addClass("password-wrong");
+            passwordMatching = false;
+        } else {
+            repasswordInput.removeClass("password-wrong")
+            passwordMatching = true;
+        }
+    })
+
+    submitButton.click((e) => {
+        e.preventDefault();
+
+        if(passwordMatching) {
+            form.submit();
+        }
+    });
 });
+
+function removeSpaces(str) {
+    return str.replace(/ /g, "");
+}
